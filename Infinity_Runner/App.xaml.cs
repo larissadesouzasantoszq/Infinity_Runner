befe7b162b7f76326f61ff2428253ab4ba61fa6a
Larissa <larissadesouzadossantossouzq@gmail.com>
2024-11-05 11:25:28 -0300

    OK

---
+++ Infinity_Runner/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Infinity_Runner;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Infinity_Runner/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Infinity_Runner;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
